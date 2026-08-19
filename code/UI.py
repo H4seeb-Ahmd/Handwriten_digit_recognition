e1d6083aed4b0679a5e1f9ac38f5b23c724ef1b7
Haseeb Ahmed <haseeb.ahmed.012018@gmail.com>
2026-08-19 16:58:05 +0530

    BnW input completed

--- code/UI.py
+++ code/UI.py
@@ -21,8 +21,10 @@ class DrawingInterface:
         self.canvas.bind("<B1-Motion>", self.paint)
         self.canvas.bind("<Button-1>", self.paint)
 
-        btn = tk.Button(self.root, text = "Process Grid", command = self.process_grid)
-        btn.pack()
+        process_btn = tk.Button(self.root, text = "Process Grid", command = self.process_grid)
+        process_btn.pack()
+        clear_btn = tk.Button(self.root, text = "Clear", command = self.clear)
+        clear_btn.pack()
 
         self.root.mainloop()
 
@@ -39,6 +41,11 @@ class DrawingInterface:
             x2 = x1 + self.cell_size
             y2 = y1 + self.cell_size
             self.canvas.create_rectangle(x1, y1, x2, y2, fill='white', outline='white')
+
+    def clear(self):
+        self.canvas.delete("all")
+        self.pixel_grid.fill(0)
+
     def process_grid(self):
         print("Grid captured! Current shape is:", self.pixel_grid.shape)
 
