45e01d1ae63b083454c1f60716a9bf44ce349f22
Haseeb Ahmed <haseeb.ahmed.012018@gmail.com>
2026-08-20 15:20:23 +0530

    dataset setup for NN

--- code/DIGIT_RECOGNIZING_MODELS.py
+++ code/DIGIT_RECOGNIZING_MODELS.py
@@ -27,6 +27,14 @@ else:
              X_test=X_test_raw, 
              y_test=y_test)
 
+def sigmoid(x):
+    # The sigmoid function
+    return 1.0 / (1.0 + np.exp(-x))
+
+def sigmoid_derivative(x):
+    # Derivative of the sigmoid
+    return sigmoid(x) * (1 - sigmoid(x))
+
 class KNN:
     def __init__(self):
         self.model_filename = 'TRAINED/knn_model.bin'
@@ -39,7 +47,7 @@ class KNN:
             with open(self.model_filename, 'rb') as file:
                 self.knn_clf = pickle.load(file)
         else:   
-            print("Training KNN Classifier...")
+            print(f"Training {self.model_nameNN} Classifier...")
             self.knn_clf = KNeighborsClassifier()
             self.knn_clf.fit(X_train, y_train)
             knn_preds = self.knn_clf.predict(X_test)
@@ -53,7 +61,7 @@ class KNN:
 
 class NeuralNetworkLib:
     def __init__(self):
-        self.model_filename = 'TRAINED/nn_model.bin'
+        self.model_filename = 'TRAINED/nnl_model.bin'
         self.model_name = "Neural Network (Library)"  
 
         X_train = X_train_raw.reshape(X_train_raw.shape[0], 28 * 28)
@@ -66,7 +74,7 @@ class NeuralNetworkLib:
             with open(self.model_filename, 'rb') as file:
                 self.nn_clf = pickle.load(file)
         else:
-            print("Training KNN Classifier...")
+            print(f"Training {self.model_nameNN} Classifier...")
             self.nn_clf = MLPClassifier(
                 hidden_layer_sizes = (128,),
                 activation = 'relu',
@@ -87,3 +95,67 @@ class NeuralNetworkLib:
 
     def model_prediction(self, entry):
             return self.nn_clf.predict(entry)
+
+class NeuralNetwork:
+    def __init__(self, sizes=[784, 16, 16, 10], epochs=10, learning_rate=3.0, batch_size=10):
+        self.model_filename = 'TRAINED/nn_model.bin'
+        self.model_name = "Neural Network" 
+
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.epochs = epochs
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+
+        self.X_train = [x.flatten().reshape(-1, 1) / 255.0 for x in X_train_raw]
+        self.X_test = [x.flatten().reshape(-1, 1) / 255.0 for x in X_test_raw]
+
+        def vectorized_result(j):
+            e = np.zeros((10, 1))
+            e[j] = 1.0
+            return e
+        
+        y_train_vec = [vectorized_result(y) for y in y_train]
+
+        self.training_data = list(zip(self.X_train, y_train_vec))
+        self.test_data = list(zip(self.X_test, y_test))
+
+        if os.path.exists(self.model_filename):
+            with open(self.model_filename, 'rb') as file:
+                saved_data = pickle.load(file)
+                self.weights = saved_data['weights']
+                self.biases = saved_data['biases']
+        else:
+            print(f"Training {self.model_name}...")
+            # Initialize random weights and biases
+            self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+            self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
+            
+            # Train the network
+            self._train()
+            
+            # Evaluate Accuracy
+            accuracy = self._evaluate(self.test_data)
+            print(f"{self.model_name} Accuracy: {accuracy / len(self.test_data) * 100:.2f}%")
+            
+            # Save the trained weights and biases
+            with open(self.model_filename, 'wb') as file:
+                pickle.dump({'weights': self.weights, 'biases': self.biases}, file)
+        
+
+        print(f"{self.model_name} model loaded successfully!")
+
+    def _train(self):
+        """Trains the network using mini-batch gradient descent."""
+        n = len(self.training_data)
+        for j in range(self.epochs):
+            # Shuffle data to prevent patterns from affecting training
+            np.random.shuffle(self.training_data)
+            
+            # Create mini-batches for faster, more stable training
+            mini_batches = [self.training_data[k:k+self.batch_size] 
+                            for k in range(0, n, self.batch_size)]
+            
+            for mini_batch in mini_batches:
+                self._update_mini_batch(mini_batch)
+            print(f"Epoch {j+1}/{self.epochs} complete")
